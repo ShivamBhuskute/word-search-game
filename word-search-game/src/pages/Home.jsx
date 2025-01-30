97b335f6d96d2027e51e0132beee15ce42b89b56
Shivam <135974981+ShivamBhuskute@users.noreply.github.com>
2025-01-30 22:03:46 +0530

    Update Home.jsx

--- word-search-game/src/pages/Home.jsx
+++ word-search-game/src/pages/Home.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import Header from "../components/Header";
-
-// const Home = () => {
-//     const navigate = useNavigate();
-
-//     const handleClick = () => {
-//         navigate("/app");
-//     };
-
-//     return (
-//         <>
-//             <Header />
-//             <div className="bg-gray-900 min-h-screen grid grid-row-3 sm:grid-cols-3">
-//                 <div className="">
-//                     <h1 className="font-bold text-5xl ">
-//                         Welcome to the Home Page
-//                     </h1>
-//                     <p>This is the content of the Home page.</p>
-//                     <button
-//                         className="mt-4 p-2 bg-blue-600 text-white rounded hover:bg-blue-100 hover:text-black"
-//                         onClick={handleClick}
-//                     >
-//                         Play
-//                     </button>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Home;
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
